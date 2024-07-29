fc792c417af9de795cd23f8a62390c0de9a4466d
VictorGotsenko <victor.gotsenko@gmail.com>
2024-07-29 18:56:05 +0500

    Add Gradle plugins and

---
+++ app/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("java")
+    id("application")
+    id("se.patrikerdes.use-latest-versions") version "0.2.18"
+    id("com.github.ben-manes.versions") version "0.41.0"
+    id("checkstyle")
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+application {
+        mainClass = "hexlet.code.App"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.3"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
