0d410855694d48259df6bf50fcd6285f6b7e8e83
VictorGotsenko <victor.gotsenko@gmail.com>
2024-08-01 16:38:48 +0500

    Add Gradle plugins and make jar-file (step 02)

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,9 +1,29 @@
+
 package hexlet.code;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Option;
+import static java.lang.System.exit;
+
+public class App implements Runnable {
+    @Option(names = {"-h", "--help"}, description = "Usage: gendiff [-hV].")
+    private boolean helpOption;
+
     public static void main(String[] args) {
+        CommandLine.run(new App(), args);
         System.out.println("Hello World!");
+    }
+    @Override
+    public void run() {
+        if (helpOption) {
+            System.out.println("Usage: gendiff [-hV] [-f=format] filepath1 filepath2\n"
+                  + "Compares two configuration files and shows a difference.\n"
+                  + "      filepath1         path to first file\n"
+                  + "      filepath2         path to second file\n"
+                  + "  -f, --format=format   output format [default: stylish]\n"
+                  + "  -h, --help      Show this help message and exit.\n"
+                  + "  -V, --version   Print version information and exit.\n");
+            exit(0);
+        }
     }
 }
