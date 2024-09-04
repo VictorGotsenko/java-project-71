e9b09f8aa40d4c8cd111ab49803d84ea6434945e
VictorGotsenko <victor.gotsenko@gmail.com>
2024-09-04 15:01:08 +0500

    Rebuild test. Before rebuild project

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,6 +1,8 @@
 // Logical module
 package hexlet.code;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -18,7 +18,8 @@ public class DifferTest {
 
     static Path getAbsolutePath(String fileName) {
         Path result = Paths.get("").toAbsolutePath();
-        Path filePath = Paths.get("src", "test", "resources", "tests", fileName);
+        Path filePath = Paths.get("src", "test", "resources", fileName);
+//        Path filePath = Paths.get("src", "test", "resources", "tests", fileName);
         return result.resolve(filePath).normalize();
     }
 
@@ -31,14 +32,51 @@ public class DifferTest {
         Assertions.assertTrue(true);
     }
 
-//    ** local my
+//    ** test 01 json without formatter
+    @Test
+    @DisplayName("Test compare nested json files whitout formatter")
+    void genDiffJsonTest01() throws Exception {
+        Assertions.assertEquals(readFixture("result_stylish(default).txt"),
+                                Differ.generate(getAbsolutePath("file1NestStrct.json").toString(),
+                                                getAbsolutePath("file2NestStrct.json").toString()));
+    }
+
+    //    ** test 02 json json -> stylish(default) formatter
+    @Test
+    @DisplayName("Test compare nested json files whit stylish formatter")
+    void genDiffJsonTest02() throws Exception {
+        Assertions.assertEquals(readFixture("result_stylish(default).txt"),
+                                Differ.generate(getAbsolutePath("file1NestStrct.json").toString(),
+                                                getAbsolutePath("file2NestStrct.json").toString(),
+                                                "stylish"));
+    }
+
+    //    ** test 03 json json -> plain formatter
+    @Test
+    @DisplayName("Test compare nested json files whit plain formatter")
+    void genDiffJsonTest03() throws Exception {
+        Assertions.assertEquals(readFixture("result_plain_format.txt"),
+                                Differ.generate(getAbsolutePath("file1NestStrct.json").toString(),
+                                                getAbsolutePath("file2NestStrct.json").toString(),
+                                                "plain"));
+    }
+
+//    ** test 04 json json -> json formatter - not working
 //    @Test
-//    @DisplayName("Test compare nested json files")
-//    void genDiffNestJsonTest() throws Exception {
-//        Assertions.assertEquals(readFixture("resultCompareNestStrct.json"),
+//    @DisplayName("Test compare nested json files whit json formatter")
+//    void genDiffJsonTest04() throws Exception {
+//        Assertions.assertEquals(readFixture("result_stylish(default).txt"),
 //                                Differ.generate(getAbsolutePath("file1NestStrct.json").toString(),
-//                                                getAbsolutePath("file2NestStrct.json").toString()));
+//                                                getAbsolutePath("file2NestStrct.json").toString(),
+//                                                "json"));
 //    }
+
+//
+
+
+
+
+
 //    **
 //** local my
 //    @Test
@@ -51,43 +89,43 @@ public class DifferTest {
 //**
 
 //  ** from Check
-    @Test
-    @DisplayName("Test compare nested json files - result_stylish")
-    void genDiffJsonStylishTest() throws Exception {
-        Assertions.assertEquals(readFixture("result_stylish.txt"),
-                                Differ.generate(getAbsolutePath("file1.json").toString(),
-                                                getAbsolutePath("file2.json").toString()));
-    }
+//    @Test
+//    @DisplayName("Test compare nested json files - result_stylish")
+//    void genDiffJsonStylishTest() throws Exception {
+//        Assertions.assertEquals(readFixture("result_stylish.txt"),
+//                                Differ.generate(getAbsolutePath("file1.json").toString(),
+//                                                getAbsolutePath("file2.json").toString()));
+//    }
 //  **
 
 //  ** from Check
-    @Test
-    @DisplayName("Test compare nested json files - result_plain ")
-    void genDiffJsonPlainTest() throws Exception {
-        Assertions.assertEquals(readFixture("result_plain.txt"),
-                                Differ.generate(getAbsolutePath("file1.json").toString(),
-                                                getAbsolutePath("file2.json").toString(), "plain"));
-    }
+//    @Test
+//    @DisplayName("Test compare nested json files - result_plain ")
+//    void genDiffJsonPlainTest() throws Exception {
+//        Assertions.assertEquals(readFixture("result_plain.txt"),
+//                                Differ.generate(getAbsolutePath("file1.json").toString(),
+//                                                getAbsolutePath("file2.json").toString(), "plain"));
+//    }
 //  **
 
 //  ** from Check
-    @Test
-    @DisplayName("Test compare nested yaml files - result_stylish ")
-    void genDiffYmlStylishTest() throws Exception {
-        Assertions.assertEquals(readFixture("result_stylish.txt"),
-                                Differ.generate(getAbsolutePath("file1.yml").toString(),
-                                                getAbsolutePath("file2.yml").toString()));
-    }
+//    @Test
+//    @DisplayName("Test compare nested yaml files - result_stylish ")
+//    void genDiffYmlStylishTest() throws Exception {
+//        Assertions.assertEquals(readFixture("result_stylish.txt"),
+//                                Differ.generate(getAbsolutePath("file1.yml").toString(),
+//                                                getAbsolutePath("file2.yml").toString()));
+//    }
 //  **
 
 //  ** from Check
-    @Test
-    @DisplayName("Test compare nested yaml files - result_plain ")
-    void genDiffYmlPlainTest() throws Exception {
-        Assertions.assertEquals(readFixture("result_plain.txt"),
-                                Differ.generate(getAbsolutePath("file1.yml").toString(),
-                                                getAbsolutePath("file2.yml").toString(), "plain"));
-    }
+//    @Test
+//    @DisplayName("Test compare nested yaml files - result_plain ")
+//    void genDiffYmlPlainTest() throws Exception {
+//        Assertions.assertEquals(readFixture("result_plain.txt"),
+//                                Differ.generate(getAbsolutePath("file1.yml").toString(),
+//                                                getAbsolutePath("file2.yml").toString(), "plain"));
+//    }
 // **
 
 /*
@@ -108,5 +146,5 @@ public class DifferTest {
                                                 getAbsolutePath("file2NestStrct.yaml").toString(),
                                                 "json"));
     }
- */
+*/
 }
